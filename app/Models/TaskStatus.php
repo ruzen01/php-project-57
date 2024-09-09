56b046663fc5a29c043b22f811ef5524207c0a43
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-09 21:15:53 +1000

    Add: Step 3, CRUD

--- app/Models/TaskStatus.php
+++ app/Models/TaskStatus.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Task;
 
 class TaskStatus extends Model
 {
     use HasFactory;
+    // Разрешаем массовое присвоение для поля 'name'
+    protected $fillable = ['name'];
+    // Определяем связь один ко многим с моделью Task
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
