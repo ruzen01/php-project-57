fb2f237cda08b6e83bee65bb033f1bb92e704c75
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-12 23:40:13 +1000

    eng to rus

--- config/database.php
+++ config/database.php
@@ -83,19 +83,17 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel_db'),
-            'username' => env('DB_USERNAME', 'laravel_user'),
+            'driver'   => 'pgsql',
+            'url'      => env('DATABASE_URL'),
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
+            'charset'  => 'utf8',
+            'prefix'   => '',
             'schema'   => 'public',
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode'  => 'prefer',
         ],
 
         'sqlsrv' => [
