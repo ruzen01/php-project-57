c7803a0b56f0d83c1edae97848e947f8219a9a9b
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-13 01:00:51 +1000

    Add: db

--- config/database.php
+++ config/database.php
@@ -86,7 +86,7 @@ return [
             'driver'   => 'pgsql',
             'url'      => env('DATABASE_URL'),
             'host'     => env('DB_HOST', '127.0.0.1'),
-            'port'     => 5432,
+            'port'     => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
