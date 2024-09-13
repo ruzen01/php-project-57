94def72c6db2d6217614dd1cf3a726298de9197c
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-14 08:47:54 +1000

    eng to rus

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,38 +40,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            <nav class="navbar navbar-expand-lg">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">Менеджер задач</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav me-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('tasks.index') }}">Задачи</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('task_statuses.index') }}">Статусы</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('labels.index') }}">Метки</a>
-                            </li>
-                        </ul>
-                        <div>
-                            @guest
-                                <a href="{{ route('login') }}" class="btn btn-primary me-2">Вход</a>
-                                <a href="{{ route('register') }}" class="btn btn-outline-primary">Регистрация</a>
-                            @else
-                                <form method="POST" action="{{ route('logout') }}" class="d-inline">
-                                    @csrf
-                                    <button type="submit" class="btn btn-outline-primary" dusk="logout-button">Выход</button>
-                                </form>
-                            @endguest
-                        </div>
-                    </div>
-                </div>
-            </nav>
+        @include('layouts.navigation')
 
             @if (session('success'))
                <div class="alert alert-success">
