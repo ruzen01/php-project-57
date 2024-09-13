bd6389e18aabb8aab77d042c79e1a8f49c55216d
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-14 09:45:51 +1000

    eng to rus

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,16 +18,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    // public function boot(): void
-    // {
-    //     if (env('FORCE_HTTPS', false)) {
-    //         URL::forceScheme('https');
-    //     }
-    // }
-
-    public function boot()
+    public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        if (env('FORCE_HTTPS', false)) {
             URL::forceScheme('https');
         }
     }
