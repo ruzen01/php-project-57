9ea0b948f751ede433d0ea2a04c5c3aa1e509ae2
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-13 21:32:53 +1000

    eng to rus

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -53,7 +53,7 @@ class TaskStatusController extends Controller
 
         $task_status->update($validated);
 
-        return redirect()->route('task_statuses.index')->with('success', 'Статус успешно изменен');
+        return redirect()->route('task_statuses.index')->with('success', 'Статус успешно изменён');
     }
 
     // Удаление статуса
