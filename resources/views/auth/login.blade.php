73efbd10a6447061c7a02b91d5668ff8fa268477
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-13 20:59:24 +1000

    eng to rus

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,9 +39,9 @@
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Войти') }}
-            </x-primary-button>
+            <button type="submit" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
+                Войти
+            </button>
         </div>
     </form>
 </x-guest-layout>
