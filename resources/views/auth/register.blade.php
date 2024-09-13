4306a6ad4e09325648aaaa2868b8a9cc84dc23b1
ruzen <155291244+ruzen01@users.noreply.github.com>
2024-09-13 02:04:12 +0000

    eng to rus

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,9 +44,12 @@
                 {{ __('Уже зарегистрированы?') }}
             </a>
 
-            <x-primary-button class="ms-4">
+            <!-- <x-primary-button class="ms-4">
                 {{ __('Зарегистрировать') }}
-            </x-primary-button>
+            </x-primary-button> -->
+            <button type="submit" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
+                Зарегистрировать
+            </button>
         </div>
     </form>
 </x-guest-layout>
