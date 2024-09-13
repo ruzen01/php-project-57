640bba8720829add745562e2ea6ed1a575545c83
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-14 08:26:46 +1000

    eng to rus

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,9 +17,9 @@
             <x-input-label for="password" :value="__('Пароль')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                type="password"
+                name="password"
+                required autocomplete="current-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
@@ -33,12 +33,12 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Забыли пароль?') }}
-                </a>
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                {{ __('Забыли пароль?') }}
+            </a>
             @endif
 
-            <button type="submit" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 transition ease-in-out duration-150 text-white font-bold py-2 px-4 rounded ml-4">
+            <button type="submit" class="font-bold py-2 px-4 rounded">
                 Войти
             </button>
         </div>
