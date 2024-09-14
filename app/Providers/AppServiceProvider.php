3a1cd3a4a5069fc20fd06ba785b07b8891f4b447
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-14 14:00:40 +1000

    1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,8 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url)
     {
-
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,27 +17,13 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @include('layouts.navigation')
 
     <style>
         /* Устанавливаем белый фон и черный текст для всего документа */
         body {
             background-color: white;
             color: black;
-        }
-
-        /* Меню навигации */
-        .navbar {
-            background-color: #f8f9fa; /* светлый фон для navbar */
-        }
-
-        .navbar-brand,
-        .nav-link {
-            color: black !important; /* черный цвет текста */
-        }
-
-        .navbar-brand:hover,
-        .nav-link:hover {
-            color: gray !important; /* серый цвет при наведении */
         }
 
         /* Стили для кнопок */
@@ -61,48 +47,6 @@
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <!-- Логотип -->
-                <a class="navbar-brand" href="{{ url('/') }}">Менеджер задач</a>
-
-                <!-- Кнопка для мобильных устройств -->
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <!-- Основное меню -->
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <!-- Навигационные ссылки -->
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('tasks.index') }}">Задачи</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('task_statuses.index') }}">Статусы</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('labels.index') }}">Метки</a>
-                        </li>
-                    </ul>
-
-                    <!-- Блок с кнопками аутентификации -->
-                    <div class="d-flex">
-                        @guest
-                        <!-- Кнопки для гостей -->
-                        <a href="{{ route('login') }}" class="btn btn-primary me-2">Вход</a>
-                        <a href="{{ route('register') }}" class="btn btn-outline-primary">Регистрация</a>
-                        @else
-                        <!-- Кнопка "Выход" для авторизованных пользователей -->
-                        <form method="POST" action="{{ route('logout') }}" class="d-inline">
-                            @csrf
-                            <button type="submit" class="btn btn-outline-primary" dusk="logout-button">Выход</button>
-                        </form>
-                        @endguest
-                    </div>
-                </div>
-            </div>
-        </nav>
 
         @if (session('success'))
         <div class="alert alert-success">
@@ -111,7 +55,6 @@
         @endif
 
         @if (session('error'))
-
         <div class="alert alert-danger">
             {{ session('error') }}
         </div>
