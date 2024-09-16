7245820273c57b693158eb08d831e0966b356fa0
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-16 23:34:35 +1000

    fix: style

--- resources/views/labels/create.blade.php
+++ resources/views/labels/create.blade.php
@@ -9,7 +9,7 @@
         <!-- Поле для имени метки -->
         <div class="mb-4">
             <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Имя</label>
-            <input type="text" name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+            <input type="text" name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             <!-- Сообщение об ошибке под полем -->
             @error('name')
             <div class="text-red-500 text-sm mt-2">{{ $message }}</div>
